adfc210887011335d35d08f667c6654af68d715f
levi Leal <levifleal@live.com>
2024-01-31 16:15:15 -0300

    fixing pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 
 //pages
 import ErrorPage from './errorPage.jsx'
